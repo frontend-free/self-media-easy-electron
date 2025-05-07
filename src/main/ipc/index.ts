5577a5a23bac8d995b5ac986f3fdd5ef03a7d8b0
liyatang <liyatang@qq.com>
2025-05-07 12:43:58 +0800

    feat: some

--- src/main/ipc/index.ts
+++ src/main/ipc/index.ts
@@ -1,65 +1,65 @@
-import { ipcMain, dialog, app } from 'electron'
-
-import { authTiktok } from './platform/tiktok/auth'
-import { publishTiktok } from './platform/tiktok/publish'
+import { app, dialog, ipcMain } from 'electron';
+import { autoUpdater } from 'electron-updater';
+import { authTiktok } from './platform/tiktok/auth';
+import { publishTiktok } from './platform/tiktok/publish';
 import {
   EnumPlatform,
   PlatformAuthResult,
   PlatformPublishParams,
   PlatformPublishResult,
-  ShowOpenDialogOfOpenFileResult
-} from './platform/types'
+  ShowOpenDialogOfOpenFileResult,
+} from './platform/types';
 
 function handlePing(): string {
-  return 'pong'
+  return 'pong';
 }
 
 async function handlePlatformAuth(_, arg?: { platform: string }): Promise<PlatformAuthResult> {
-  console.log('handlePlatformAuth', arg)
+  console.log('handlePlatformAuth', arg);
 
   if (!arg || !arg.platform) {
     return {
       success: false,
-      message: '平台不能为空'
-    }
+      message: '平台不能为空',
+    };
   }
 
-  const { platform } = arg
+  const { platform } = arg;
 
   switch (platform) {
     case EnumPlatform.TIKTOK:
-      return await authTiktok()
+      return await authTiktok();
     default:
       return {
         success: false,
-        message: `平台不支持 ${platform}`
-      }
+        message: `平台不支持 ${platform}`,
+      };
   }
 }
 
 async function handlePlatformPublish(
   _,
-  arg?: PlatformPublishParams
+  arg?: PlatformPublishParams,
 ): Promise<PlatformPublishResult> {
-  console.log('handlePlatformPublish', arg)
+  console.log('handlePlatformPublish', arg);
 
   if (!arg || !arg.platform || !arg.authInfo || !arg.resourceOfVideo) {
     return {
       success: false,
-      message: '参数错误，请检查'
-    }
+      message: '参数错误，请检查',
+    };
   }
 
-  const { platform } = arg
+  const { platform } = arg;
 
   switch (platform) {
     case EnumPlatform.TIKTOK:
-      return await publishTiktok(arg)
+      return await publishTiktok(arg);
     default:
       return {
         success: false,
-        message: `平台不支持 ${platform}`
-      }
+        message: `平台不支持 ${platform}`,
+      };
   }
 }
 
@@ -69,35 +69,41 @@ async function handleShowOpenDialogOfOpenFile(): Promise<ShowOpenDialogOfOpenFil
     filters: [
       {
         name: '视频',
-        extensions: ['mp4', 'avi', 'mkv']
-      }
-    ]
-  })
+        extensions: ['mp4', 'avi', 'mkv'],
+      },
+    ],
+  });
 
   return {
     success: true,
     data: {
-      filePaths: result.filePaths || []
-    }
-  }
+      filePaths: result.filePaths || [],
+    },
+  };
 }
 
 function handleGetVersion(): string {
-  return app.getVersion()
+  return app.getVersion();
+}
+
+async function handleCheckForUpdatesAndNotify(): Promise<void> {
+  await autoUpdater.checkForUpdatesAndNotify();
 }
 
 function initIpc(): void {
   // test
-  ipcMain.handle('ping', handlePing)
+  ipcMain.handle('ping', handlePing);
 
   // 获取版本号
-  ipcMain.handle('getVersion', handleGetVersion)
+  ipcMain.handle('getVersion', handleGetVersion);
 
-  ipcMain.handle('platformAuth', handlePlatformAuth)
+  ipcMain.handle('checkForUpdatesAndNotify', handleCheckForUpdatesAndNotify);
 
-  ipcMain.handle('platformPublish', handlePlatformPublish)
+  ipcMain.handle('platformAuth', handlePlatformAuth);
 
-  ipcMain.handle('showOpenDialogOfOpenFile', handleShowOpenDialogOfOpenFile)
+  ipcMain.handle('platformPublish', handlePlatformPublish);
+
+  ipcMain.handle('showOpenDialogOfOpenFile', handleShowOpenDialogOfOpenFile);
 }
 
-export { initIpc }
+export { initIpc };
